93afc9a76a149c32efdf4259631d27e1e43ded91
Zadkiel Aslafy-Aharonian <aslafy_z@epitech.eu>
2017-02-24 20:13:34 +0000

    Added patched rancher-mongo-config

---
+++ mongodb-config/connect.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+if [ -n "$CATTLE_SCRIPT_DEBUG" ]; then
+	set -x
+fi
+
+GIDDYUP=/opt/rancher/bin/giddyup
+
+function cluster_init {
+	sleep 10
+	MYIP=$($GIDDYUP ip myip)
+	mongo --eval "printjson(rs.initiate())"
+	for member in $($GIDDYUP ip stringify --delimiter " "); do
+		if [ "$member" != "$MYIP" ]; then
+			mongo --eval "printjson(rs.add('$member:27017'))"
+			sleep 5
+		fi
+	done
+
+}
+
+function find_master {
+	for member in $($GIDDYUP ip stringify --delimiter " "); do
+		IS_MASTER=$(mongo --host $member --eval "printjson(db.isMaster())" | grep 'ismaster')
+		if echo $IS_MASTER | grep "true"; then
+			return 0
+		fi
+	done
+	return 1
+}
+# Script starts here
+# wait for mongo to start
+$GIDDYUP service wait scale --timeout 120
+
+# Wait until all services are up
+sleep 10
+find_master
+if [ $? -eq 0 ]; then
+	echo 'Master is already initated.. nothing to do!'
+else
+	echo 'Initiating the cluster!'
+	cluster_init
+fi
---
+++ mongodb-config/scaling.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [ -n "$CATTLE_SCRIPT_DEBUG" ]; then
+	set -x
+fi
+
+sleep 5
+GIDDYUP=/opt/rancher/bin/giddyup
+
+function scaleup {
+	MYIP=$($GIDDYUP ip myip)
+	for IP in $($GIDDYUP ip stringify --delimiter " "); do
+		IS_MASTER=$(mongo --host $IP --eval "printjson(db.isMaster())" | grep 'ismaster')
+		if echo $IS_MASTER | grep "true"; then
+			mongo --host $IP --eval "printjson(rs.add('$MYIP:27017'))"
+			return 0
+		fi
+	done
+	return 1
+}
+
+# Script starts here
+if [ $($GIDDYUP service scale --current) -gt 3 ]; then
+	scaleup
+fi
