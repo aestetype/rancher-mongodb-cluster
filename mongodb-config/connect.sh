73383de2083902ce8b3d1e42bc632f3c9d7fb872
Zadkiel Aslafy-Aharonian <aslafy_z@epitech.eu>
2017-03-02 16:50:33 +0000

    Use hostnames instead of ips

--- mongodb-config/connect.sh
+++ mongodb-config/connect.sh
@@ -8,15 +8,16 @@ GIDDYUP=/opt/rancher/bin/giddyup
 
 function cluster_init {
 	sleep 10
-	MYIP=$($GIDDYUP ip myip)
-	mongo --eval "printjson(rs.initiate())"
-	for member in $($GIDDYUP ip stringify --delimiter " "); do
-		if [ "$member" != "$MYIP" ]; then
-			mongo --eval "printjson(rs.add('$member:27017'))"
+	# MYIP=$($GIDDYUP ip myip)
+  # Using hostname instead of ips to solve this: https://jira.mongodb.org/browse/NODE-746
+  MYHOSTNAME=$($GIDDYUP ip myname)
+	mongo --eval "printjson(rs.initiate({_id :'rs0', members:[{_id:0, host:'$MYHOSTNAME.rancher.internal:27017'}]}))"
+	for member in $($GIDDYUP ip stringify --use-container-names --delimiter " "); do
+		if [ "$member" != "$MYHOSTNAME" ]; then
+			mongo --eval "printjson(rs.add('$member.rancher.internal:27017'))"
 			sleep 5
 		fi
 	done
-
 }
 
 function find_master {
--- mongodb-config/scaling.sh
+++ mongodb-config/scaling.sh
@@ -8,11 +8,13 @@ sleep 5
 GIDDYUP=/opt/rancher/bin/giddyup
 
 function scaleup {
-	MYIP=$($GIDDYUP ip myip)
+	# MYIP=$($GIDDYUP ip myip)
+  # Using hostname instead of ips to solve this: https://jira.mongodb.org/browse/NODE-746
+  MYHOSTNAME=$($GIDDYUP ip myname)
 	for IP in $($GIDDYUP ip stringify --delimiter " "); do
 		IS_MASTER=$(mongo --host $IP --eval "printjson(db.isMaster())" | grep 'ismaster')
 		if echo $IS_MASTER | grep "true"; then
-			mongo --host $IP --eval "printjson(rs.add('$MYIP:27017'))"
+			mongo --host $IP --eval "printjson(rs.add('$MYHOSTNAME.rancher.internal:27017'))"
 			return 0
 		fi
 	done
